c3c510da4c1b097c71fca1a04dc5167346294be2
Stephen Haddad <stephen.haddad@informaticslab.co.uk>
2020-10-23 16:10:08 +0000

    XBT #84 part way througb updates to be able to run experiments using the azureML hyperdrive hyperparameter tuning tool.

--- xbt/azureml.py
+++ xbt/azureml.py
@@ -50,8 +50,12 @@ class AzureExperiment:
     def _write_exp_outputs_to_azml(self):
         # upload small files to the run
         if self.scores_out_path:
-            self._azml_run.log_table('scores',
-                                     {c1: list(self.score_table[c1]) for c1 in self.score_table.columns})
+
+# turned off all the logging because it it error prone currently, because of the 3kb limit
+# need to rethink how this is done as I'm clearly not using this functionality as it is intended.
+# for now, the same data is available as a CSV file uploaded to the run
+#             self._azml_run.log_table('scores',
+#                                      {c1: list(self.score_table[c1]) for c1 in self.score_table.columns})
             print(f'uploading scores file {self.scores_out_path} to AzML run')
             self._azml_run.upload_file(os.path.split(self.scores_out_path)[1],
                                        self.scores_out_path)
@@ -97,11 +101,16 @@ class AzureCvExperiment(xbt.experiment.CVExperiment, AzureExperiment):
         self._write_exp_outputs_to_azml()
 
 
-class AzureCvExperiment(xbt.experiment.CvhptExperiment, AzureExperiment):
-    def __init__(self, json_descriptor, data_dir, output_dir, output_split, do_preproc_extract=False):
+class AzureCvhptExperiment(xbt.experiment.CvhptExperiment, AzureExperiment):
+    def __init__(self, json_descriptor, data_dir, output_dir, output_split, do_preproc_extract=False, add_hpt={}):
         self._get_azure_context()
+        self._additional_hyperparameters = add_hpt
         super().__init__(json_descriptor, data_dir, output_dir, output_split, do_preproc_extract)
 
+    def _overwrite_hyperparameters(self):
+        """
+        Overwrite hyperparameters read in from the JSON file with those entered through the command line arguments.
+        """
     def run_experiment(self, write_results=True, write_predictions=True, export_classifiers=True):
         super().run_experiment(write_results, write_predictions, export_classifiers)
         self._write_exp_outputs_to_azml()
@@ -116,8 +125,10 @@ class AzureInferenceExperiment(xbt.experiment.InferenceExperiment, AzureExperime
         super().run_experiment(write_results, write_predictions, export_classifiers)
         self._write_exp_outputs_to_azml()
 
+        
+class AzureExperimentLauncher(xbt.experiment.)
 
-def get_arguments(description):
+def _get_parser(description):
     parser = argparse.ArgumentParser(description=description)
     help_msg = 'The path to the JSON file containing the experiment definition.'
     parser.add_argument('--json-experiment', dest='json_experiment', help=help_msg, required=True)
@@ -153,8 +164,14 @@ def get_arguments(description):
                         dest='output_root',
                         help=help_msg,
                         )
+
+def get_arguments(description):
+    parser = _get_parser(description)
     return parser.parse_args()
 
+def get_partial_arguments(description):
+    parser = _get_parser(description)
+    return parser.parse_known_args()
 
 def run_azml_experiment():
     exp_args = get_arguments(
@@ -185,3 +202,79 @@ def run_azml_experiment():
         return_code = 1
 
     return return_code
+
+def run_azml_cv_experiment():
+    exp_args = get_arguments(
+        'Run training, inference and evaluation using cross-validation.'
+    )
+    return_code = 0
+
+    json_desc_path = os.path.join(exp_args.config_dir, exp_args.json_experiment)
+    output_dir = os.path.join(exp_args.output_root,
+                              exp_args.output_dir,
+                              )
+
+    print(f'input directory {exp_args.input_dir}')
+    print(f'output directory {exp_args.output_dir}')
+    print(f'json experiment path {json_desc_path}')
+
+    xbt_exp = AzureCvExperiment(
+        json_descriptor=json_desc_path,
+        data_dir=exp_args.input_dir,
+        output_dir=output_dir,
+        do_preproc_extract=exp_args.do_preproc_extract,
+        output_split=exp_args.output_file_split,
+    )
+    try:
+        xbt_exp.run_experiment()
+    except RuntimeError as e1:
+        print(f'Runtime error:\n {str(e1)}')
+        return_code = 1
+
+    return return_code
+
+
+def run_azml_cvhpt_experiment():
+    exp_args, hpts = get_partial_arguments('Run training, inference and evaluation using cross-validation and hyperparameter tuning.')
+    
+    
+    hpt_dict = {}
+    for ix1,par1 in enumerate(hpts):
+        if par1[:2] == '--':
+            try:
+                hpt_dict[par1[2:]] == hpts[ix1+1]
+            except IndexError:
+                print('Incorrectly entered hyperparameter arguments.')
+    
+    
+    return_code = 0
+
+                        
+    json_desc_path = os.path.join(exp_args.config_dir, exp_args.json_experiment)
+    output_dir = os.path.join(exp_args.output_root,
+                              exp_args.output_dir,
+                              )
+
+    print(f'input directory {exp_args.input_dir}')
+    print(f'output directory {exp_args.output_dir}')
+    print(f'json experiment path {json_desc_path}')
+
+    # TODO: pass in overide parameters as an argument to the class
+    # TODO: create a separate function for running cvhpt in one job vs jobs that are being called from azureML hyperdrive
+    # TODO: create a separate class for running cvhpt in one job vs jobs that are being called from azureML hyperdrive
+    # TODO: class must override the read json function,, which calls the base class version, then overrides any hyperparameters that have been defined on the command line (this will have been done by the hyperparameter tuning)
+    xbt_exp = AzureCvhptExperiment(
+        json_descriptor=json_desc_path,
+        data_dir=exp_args.input_dir,
+        output_dir=output_dir,
+        do_preproc_extract=exp_args.do_preproc_extract,
+        output_split=exp_args.output_file_split,
+    )
+    try:
+        xbt_exp.run_experiment()
+    except RuntimeError as e1:
+        print(f'Runtime error:\n {str(e1)}')
+        return_code = 1
+
+    return return_code
+
