5f22fde6c86db4f9208cbe2d41bca1265bf72f3c
stevehadd <stevehadd@yahoo.com>
2020-12-10 17:55:38 +0000

    XBT #80 some addition to spice stuff dfor processing WOD data, which Rachel will need to do.

--- slurm/run_spice_experiment.sh
+++ slurm/run_spice_experiment.sh
@@ -2,4 +2,4 @@
 
 module load scitools/experimental-current
 echo "Running xbt experiment"
-${XBT_REPO_ROOT}/bin/${XBT_EXEC} --json-experiment ${XBT_JSON_EXP} --input-path ${XBT_INPUT_PATH} --output-path ${XBT_OUTPUT_PATH}
+${XBT_REPO_ROOT}/bin/${XBT_EXEC} --json-experiment ${XBT_JSON_EXP} --input-path ${XBT_INPUT_PATH} --output-path ${XBT_OUTPUT_PATH} ${XBT_PREPROC_ARG}
